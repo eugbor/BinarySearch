9376284909b02337bdc515d458dae638ede4ecc8
eugbor <eugeneborodin@mail.ru>
2016-10-03 13:59:40 +0300

    commit

---
+++ BinarySearch/Car.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    public class Car
+    {
+        private readonly Circle[] _circles;
+
+        private readonly Engine _engine;
+
+        public Car()
+        {
+            _circles = new Circle[4];
+            for (int i = 0; i < 4; i++)
+                _circles[i] = new Circle();
+
+            _engine = new Engine(100);
+
+            _engine.OnRun += EngineOnOnRun;
+        }
+
+        private void EngineOnOnRun(int speed)
+        {
+            foreach (Circle circle in _circles)
+                circle.Speed = speed;
+        }
+
+        public void Go()
+        {
+            _engine.Run();
+        }
+
+        public void Left()
+        {
+            
+        }
+
+        public void Right()
+        {
+            
+        }
+    }
+
+    public class Kran: Car
+    {
+        private Arrow _arrow;
+
+        public Kran()
+        {
+            _arrow = new Arrow();
+        }
+    }
+
+    public class Arrow
+    {
+        public int Lenght { get; set; }
+    }
+
+    public class Engine
+    {
+        public int Power { get; private set; }
+
+        public event Action<int> OnRun;
+
+        public Engine(int power)
+        {
+            Power = power;
+        }
+
+        public void Run()
+        {
+            OnRun(new Random().Next(10) * Power);
+        }
+    }
+
+    public class Circle
+    {
+        public int Speed { get; set; }
+    }
+}
--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -4,39 +4,84 @@ namespace BinarySearch
 {
     class Program
     {
+        /// <summary>
+        /// Алгоритм бинарного (двоичного) поиска (2 способа: рекурсивный и нерекурсивный).
+        /// Вычесление времени решения задачи.
+        /// Вывод Debug на консоль.
+        /// Полиморфизм.
+        /// </summary>
         static void Main(string[] args)
         {
+            //var car = new Car();
+            //car.Go();
+
             int n = int.Parse(Console.ReadLine());
-            var Array = new int[n];
-            SetArray(Array);
-            Print(Array);
-            SortArray(Array);
-            Print(Array);
-            SearchValue(Array);
+            var Arr = new int[n];
+            SetArray(Arr);
+            Print(Arr);
+            SortArray(Arr);
+            Print(Arr);
+
+            var A = SearchArray(Arr, 0, Arr.Length-1, 5);
+            Console.WriteLine("{0}", A);
+
+            Console.WriteLine("{0}", SearchValue(Arr));
+
+
+            int countIteration = 1000;
+            var start = DateTime.Now;
+            for (int i = 0; i < countIteration; i++)
+            {
+                var index = SearchValue(Arr);
+            }
+            Console.WriteLine((DateTime.Now - start).TotalMilliseconds / 1000);
+
+            var start1 = DateTime.Now;
+            for (int i = 0; i < countIteration; i++)
+            {
+                var index = SearchArray(Arr, 0, Arr.Length - 1, 5);
+            }
+            Console.WriteLine((DateTime.Now - start1).TotalMilliseconds / 1000);
+
+
             Console.ReadKey();
         }
 
-        public static void SearchValue(int[] a)
+        public static int SearchArray(int[] array, int startIndex, int endIndex, int value) // рекурсивный
+        {
+            //Debug.WriteLine("{0} {1}", startIndex, endIndex);
+            int average = startIndex + (endIndex - startIndex)/2;
+
+            if (endIndex <= startIndex)
+                return -1;
+
+            if (array[average] > value)
+                return SearchArray(array, startIndex, average, value);
+
+            if (array[average] == value)
+                return average;
+
+            return SearchArray(array, average + 1, endIndex, value);
+        }
+
+        public static int SearchValue(int[] a) // нерекурсивный
         {
             int averageIndex = 0; // переменная для хранения индекса среднего элемента массива
             int firstIndex = 0; // индекс первого элемента в массиве
             int lastIndex = a.Length - 1; // индекс последнего элемента в массиве
             int searchValue = 5; // искомое (ключевое) значение
 
-            if (lastIndex == -1) Console.WriteLine("\narray is empty"); // массив пуст
-
-            while (firstIndex < lastIndex)
+            for (int i = 0; i < a.Length; i++)
             {
-                averageIndex = firstIndex + (lastIndex - firstIndex)/2; // меняем индекс среднего значения
+                averageIndex = firstIndex + (lastIndex - firstIndex) / 2; // меняем индекс среднего значения
                 if (searchValue <= a[averageIndex]) // найден ключевой элемент или нет
                     lastIndex = averageIndex;
                 else
-                    firstIndex = averageIndex + 1; 
+                    firstIndex = averageIndex + 1;
             }
             if (a[lastIndex] == searchValue)
-                    Console.WriteLine("\nvalue is found \nindex = {0}", lastIndex);
-            else
-                    Console.WriteLine("\nvalue is not found");
+                return lastIndex;
+            return -1;
         }
 
         public static void SortArray(int[] a)
@@ -45,7 +90,7 @@ namespace BinarySearch
             {
                 for (int j = i + 1; j < a.Length; j++)
                 {
-                    if (!(a[i] < a[j]))
+                    if (a[i] > a[j])
                     {
                         int b = a[i];
                         a[i] = a[j];
